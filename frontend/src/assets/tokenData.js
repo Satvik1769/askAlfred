d92602365990b232bfbc03aae13d1e9c623f1db2
Satvik1769 <manchandasatvik@gmail.com>
2024-09-21 18:26:05 +0530

    feature: search token

---
+++ frontend/src/assets/tokenData.js
@@ -0,0 +1,367 @@
+const tokenData = {
+  1: {
+    symbol: "ETH",
+    name: "Ether",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 1,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    isFoT: false,
+    chain: "0x1",
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:ETH",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:ETH",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  42161: {
+    symbol: "ETH",
+    name: "Ether",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 42161,
+    chain: "0xa4b1",
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:ETH",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:ETH",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  10: {
+    symbol: "ETH",
+    name: "Ether",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 10,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    isFoT: false,
+    chain: "0xa",
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:ETH",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:ETH",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  324: {
+    symbol: "ETH",
+    name: "Ether",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 324,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    chain: "0x144",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:ETH",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:ETH",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  8453: {
+    symbol: "ETH",
+    name: "Ether",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 8453,
+    decimals: 18,
+    chain: "0x2105",
+    logoURI:
+      "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:ETH",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:ETH",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  56: {
+    symbol: "BNB",
+    name: "BNB",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 56,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c.png",
+    chain: "0x38",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:BNB",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:BNB",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  137: {
+    symbol: "MATIC",
+    name: "MATIC",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 137,
+    decimals: 18,
+    chain: "0x89",
+    logoURI:
+      "https://tokens.1inch.io/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:MATIC",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:MATIC",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  100: {
+    symbol: "xDAI",
+    name: "xDAI",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 100,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee_1.png",
+    chain: "0x64",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:USD",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  43114: {
+    symbol: "AVAX",
+    name: "Avalanche",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 43114,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7.png",
+    chain: "0xa86a",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "crosschain",
+        provider: "1inch",
+      },
+      {
+        value: "GROUP:AVAX",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:AVAX",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  250: {
+    symbol: "FTM",
+    name: "Fantom",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 250,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0x4e15361fd6b4bb609fa63c81a2be19d873717870.png",
+    isFoT: false,
+    chain: "0xfa",
+    rating: 2,
+    eip2612: false,
+    tags: [
+      {
+        value: "GROUP:FTM",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:FTM",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  8217: {
+    symbol: "KLAY",
+    name: "Klaytn",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 8217,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xe4f05a66ec68b54a58b17c22107b02e0232cc817.png",
+    isFoT: false,
+    rating: 2,
+    chain: "0x2019",
+    eip2612: false,
+    tags: [
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:KLAY",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+  1313161554: {
+    symbol: "ETH",
+    name: "Ether",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    chainId: 1313161554,
+    decimals: 18,
+    logoURI:
+      "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    isFoT: false,
+    rating: 2,
+    eip2612: false,
+    chain: "0x4E454152",
+    tags: [
+      {
+        value: "GROUP:ETH",
+        provider: "1inch",
+      },
+      {
+        value: "native",
+        provider: "1inch",
+      },
+      {
+        value: "PEG:ETH",
+        provider: "1inch",
+      },
+    ],
+    providers: ["1inch", "Curve Token List"],
+  },
+};
+export default tokenData;
